409fcdc3bbc9e3756ba4b97c78c96cf70b3ccb12
umayanga <umayangag@gmail.com>
2022-01-07 15:44:08 +0530

    config update

--- config.go
+++ config.go
@@ -3,6 +3,10 @@ package config
 /**
 Set the GIG server API url here for crawlers
  */
-var ApiUrl = "http://localhost:9000/api/"
-var NERServerUrl = "http://localhost:8080/classify"
-var NormalizeServer = "http://localhost:9000/api/normalize"
+
+//var ServerUrl = "https://api.gigdemo.opensource.lk"
+var ServerUrl = "http://localhost"
+var ApiUrl = ServerUrl + ":9000/api/"
+var NERServerUrl = "http://18.221.69.238:8080/classify"
+var NormalizeServer = ApiUrl + "normalize"
+var OCRServer = ServerUrl + ":8081/extract?url="
