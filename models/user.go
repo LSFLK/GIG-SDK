b50b0850d490080c7666fe8774942068365f70be
umayanga <umayangag@gmail.com>
2022-01-31 15:31:32 +0530

    define user entity

---
+++ models/user.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+type User struct {
+	Id       bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Name     string        `json:"name" bson:"name"`
+	Email    string        `json:"email" bson:"email"`
+	Role     string        `json:"role" bson:"role"`
+	Password []byte        `json:"-"`
+}
+
+func (e User) GetId() bson.ObjectId {
+	return e.Id
+}
