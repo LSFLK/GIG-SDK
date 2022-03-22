5e4dd9d5af4815039050841b69cd1eeda6247284
umayanga <umayangag@gmail.com>
2022-03-22 18:00:19 +0530

    auto format

--- tests/client/config.go
+++ tests/client/config.go
@@ -7,9 +7,9 @@ Set the GIG server API url here for crawlers
  */
 
 var testClient = GIG_SDK.GigClient{
-ApiUrl:                 "http://localhost:9000/api/",
-ApiKey:                 "[ApiKey]",
-NerServerUrl:           "http://localhost:8080/classify",
-NormalizationServerUrl: "http://localhost:9000/api/",
-OcrServerUrl:           "http://localhost:8081/extract?url=",
+	ApiUrl:                 "http://localhost:9000/api/",
+	ApiKey:                 "[ApiKey]",
+	NerServerUrl:           "http://localhost:8080/classify",
+	NormalizationServerUrl: "http://localhost:9000/api/",
+	OcrServerUrl:           "http://localhost:8081/extract?url=",
 }
