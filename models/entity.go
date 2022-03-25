487b7f31a1e47018160f2b18dd802e77a8f0042b
umayanga <umayangag@gmail.com>
2022-03-25 12:34:13 +0530

    entity model

--- models/entity.go
+++ models/entity.go
@@ -10,11 +10,11 @@ import (
 	"time"
 )
 
+// swagger:model
 /**
 It is recommended to use get,set functions to access values of the entity.
 Directly modify attributes only if you know what you are doing.
  */
-// swagger:model
 type Entity struct {
 	Id              bson.ObjectId        `json:"-" bson:"_id,omitempty"`
 	Title           string               `json:"title" bson:"title"`
